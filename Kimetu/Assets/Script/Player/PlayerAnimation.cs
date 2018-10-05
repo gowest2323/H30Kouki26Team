78601acc290eefb2ef3e5f7de1bbdcc8ddbf1139
VANO0125 <43366986+VANO0125@users.noreply.github.com>
2018-10-05 14:49:09 +0900

    PlayerAnimation.csの実装

---
+++ Kimetu/Assets/Script/Player/PlayerAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation  {   
+     private Animator anim;
+    
+    public PlayerAnimation(Animator anim)
+    {
+        this.anim = anim;
+    }
+	// Use this for initialization
+
+   public void StartRunAnimation()
+    {
+        anim.SetBool("Run",true);
+    }
+
+    public void StopRunAnimation()
+    {
+        anim.SetBool("Run", false);
+    }
+
+
+}
