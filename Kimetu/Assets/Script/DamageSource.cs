99479a98688f2ef592cbd5bae5fc86354474b479
Koya <koya0306@outlook.jp>
2018-10-05 15:20:34 +0900

    DamageSource, IDamageableを追加

---
+++ Kimetu/Assets/Script/DamageSource.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageSource {
+	public Vector3 attackPosition { private set; get; }
+	public int damage { private set; get; }
+
+	public DamageSource(Vector3 attackPosition, int damage) {
+		this.attackPosition = attackPosition;
+		this.damage = damage;
+	}
+
+	public static DamageSource Create(Vector3 attackPosition, int damage) {
+		//必要ないけど設計書に合わせて
+		return new DamageSource(attackPosition, damage);
+	}
+}
