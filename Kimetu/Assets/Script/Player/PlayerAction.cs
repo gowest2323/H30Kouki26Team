249ddbd992fcabb4749d4cc37c3d1c8997042918
Koya <koya0306@outlook.jp>
2018-10-05 13:41:31 +0900

    キャラクターが動くように

---
+++ Kimetu/Assets/Script/Player/PlayerAction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction {
+	private Transform transform;
+
+	public PlayerAction(Transform transform) {
+		this.transform = transform;
+	}
+
+	public void Move(Vector3 dir) {
+		//こうしないとコントローラのスティックがニュートラルに戻った時、
+		//勝手に前を向いてしまう
+		if(dir == Vector3.zero) {
+			//playerAnimationManager.StopWalkAnimation();
+			return;
+		}
+		//playerAnimationManager.StartWalkAnimation();
+		var pos = transform.position;
+		//transform.position += dir * 10 * Slow.Instance.playerDeltaTime;
+		transform.position += dir * 10 * Time.deltaTime;
+		transform.rotation = Quaternion.LookRotation(dir, Vector3.up);
+	}
+}
---
+++ Kimetu/Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	private PlayerAction action;
+
+	// Use this for initialization
+	void Start () {
+		this.action = new PlayerAction(transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var dir = new Vector3(
+			Input.GetAxis("Horizontal"),
+			0,
+			Input.GetAxis("Vertical")
+		);
+		action.Move(dir);
+	}
+}
