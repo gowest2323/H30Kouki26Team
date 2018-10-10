87a983e95d13ff5102ee381e920c59d1f8b70bbd
Koya <koya0306@outlook.jp>
2018-10-10 20:27:40 +0900

    AIの追加

---
+++ Kimetu/Assets/Script/AI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// エネミーの挙動を制御するクラスです。
+/// </summary>
+public abstract class AI : MonoBehaviour {
+
+	// Use this for initialization
+	public void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+		
+	}
+}
