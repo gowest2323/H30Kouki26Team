73f08808023591bcefb4c73309348eecd46f1406
Koya <koya0306@outlook.jp>
2018-10-10 20:24:45 +0900

    CharacterAnimationの追加

---
+++ Kimetu/Assets/Script/CharacterAnimation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class CharacterAnimation : MonoBehaviour {
+	/// <summary>
+	/// アニメーションの速度を 0-1 の範囲で設定します。
+	/// </summary>
+	/// <value></value>
+	public float speed {
+		set {
+			Init();
+			animator.speed = value;
+		}
+		get {
+			Init();
+			return animator.speed;
+		}
+	}
+	private Animator animator;
+
+	// Use this for initialization
+	public void Start () {
+		Init();
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+		
+	}
+
+	private void Init() {
+		if(this.animator != null) {
+			return;
+		}
+		this.animator = GetComponent<Animator>();
+		Assert.IsTrue(animator != null, "Animatorコンポーネントがありません:" + name);
+	}
+}
