967611be638277ceb09cc067b3d2390c3710775e
Koya <koya0306@outlook.jp>
2018-10-11 17:56:38 +0900

    MonoBehaivourを継承させてしまうと変更範囲が大きくなってしまうのでインターフェイスに変更

--- Kimetu/Assets/Script/CharacterAnimation.cs
+++ Kimetu/Assets/Script/CharacterAnimation.cs
@@ -3,38 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-public class CharacterAnimation : MonoBehaviour {
+public interface CharacterAnimation {
 	/// <summary>
 	/// アニメーションの速度を 0-1 の範囲で設定します。
 	/// </summary>
 	/// <value></value>
-	public float speed {
-		set {
-			Init();
-			animator.speed = value;
-		}
-		get {
-			Init();
-			return animator.speed;
-		}
-	}
-	private Animator animator;
-
-	// Use this for initialization
-	public void Start () {
-		Init();
-	}
-	
-	// Update is called once per frame
-	public void Update () {
-		
-	}
-
-	private void Init() {
-		if(this.animator != null) {
-			return;
-		}
-		this.animator = GetComponent<Animator>();
-		Assert.IsTrue(animator != null, "Animatorコンポーネントがありません:" + name);
+	float speed {
+		set; get;
 	}
 }
