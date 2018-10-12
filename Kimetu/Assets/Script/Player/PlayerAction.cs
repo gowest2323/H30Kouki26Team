5a023677aa4f56419c85e320708bc622553a9777
Koya <koya0306@outlook.jp>
2018-10-12 10:08:53 +0900

    PlayerAction にコマンドを追加

--- Kimetu/Assets/Script/Player/PlayerAction.cs
+++ Kimetu/Assets/Script/Player/PlayerAction.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlayerAction {
 	private PlayerAnimation playerAnimation;
 	private Transform transform;
+	private PlayerState state;
+	private bool isGuard;
+	private float counterTime;
+	private float counterOccuredTime;
 
 	public PlayerAction(PlayerAnimation playerAnimation, Transform transform) {
 		this.playerAnimation = playerAnimation;
@@ -25,7 +29,38 @@ public class PlayerAction {
 		transform.rotation = Quaternion.LookRotation(dir, Vector3.up);
 	}
 
+	/// <summary>
+	/// 攻撃を開始します。
+	/// </summary>
 	public void Attack() {
 		playerAnimation.StartAttackAnimation();
 	}
+
+	/// <summary>
+	/// ガードを開始します。
+	/// </summary>
+	public void GuardStart() {
+		this.isGuard = true;
+		this.state = PlayerState.Defence;
+		//TODO:ここでガードモーションに入る
+	}
+
+	/// <summary>
+	/// ガードを終了します。
+	/// </summary>
+	public void GuardEnd() {
+		this.isGuard = false;
+		this.state = PlayerState.Idle;
+	}
+
+	/// <summary>
+	/// 回避行動を実行します。
+	/// 数秒後に自動で回避状態は解除されます。
+	/// </summary>
+	public void Avoid() {
+		this.state = PlayerState.Avoid;
+		//TODO:コルーチンなどを開始する
+		//TODO:回避行動中は他のアクションを実行できないように
+	}
+
 }
