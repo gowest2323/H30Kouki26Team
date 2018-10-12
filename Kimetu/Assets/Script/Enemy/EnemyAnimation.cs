46376f9a80de6869be68ac8d83f1ea63becb90e0
VANO0125 <43366986+VANO0125@users.noreply.github.com>
2018-10-12 10:34:05 +0900

    EnemyAnimationの実装
    とりあえず、RunとAttackだけ書いてあります

---
+++ Kimetu/Assets/Script/Enemy/EnemyAnimation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimation :CharacterAnimation {
+
+    private Animator enemyAnim;
+    public float speed
+    {
+        get
+        {
+            return enemyAnim.speed;
+        }
+
+        set
+        {
+            enemyAnim.speed = value;
+        }
+    }
+
+    public EnemyAnimation(Animator enemyAnim)
+    {
+        this.enemyAnim = enemyAnim;
+        this.speed = 1.0f;
+    }
+    public void StartRunAnimation()
+    {
+        enemyAnim.SetBool("Run", true);
+    }
+
+    public void StopRunAnimation()
+    {
+        enemyAnim.SetBool("Run", false);
+    }
+
+    public void StartAttackAnimation()
+    {
+        enemyAnim.SetTrigger("Attack");
+    }
+
+
+
+
+}
