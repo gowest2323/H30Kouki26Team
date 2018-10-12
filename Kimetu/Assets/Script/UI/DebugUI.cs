6ded67b1e7b89f400a1bccbb5f8e89ca25da4235
Koya <koya0306@outlook.jp>
2018-10-12 14:29:06 +0900

    デバッグ用のUI(HP, SP表示用)を追加

---
+++ Kimetu/Assets/Script/UI/DebugUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+public class DebugUI : MonoBehaviour {
+	[SerializeField]
+	private Text hpText;
+
+	[SerializeField]
+	private Text spText;
+
+	[SerializeField]
+	private PlayerStatus status;
+
+	// Use this for initialization
+	void Start () {
+		Assert.IsTrue(hpText != null);
+		Assert.IsTrue(spText != null);
+		Assert.IsTrue(status != null);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		hpText.text = "HP: " + status.GetHP();
+		spText.text = "SP: " + status.GetStamina();
+	}
+}
