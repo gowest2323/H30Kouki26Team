20787d76c29b12501adb75ca2d76643519e6002e
KPC-017A1377\K017A1377 <matsumoto.k017a1377@gmail.com>
2018-10-12 09:28:34 +0900

    キャラクターの持つべき振る舞いを抽象化したクラスを追加

---
+++ Kimetu/Assets/Script/CharacterBehviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharacterBehviour
+{
+    protected float speed; //移動速度
+
+    /// <summary>
+    /// 移動処理
+    /// </summary>
+    public abstract void Move(); 
+
+    /// <summary>
+    /// 攻撃処理
+    /// </summary>
+    public abstract void Attack();
+
+    /// <summary>
+    /// カウンターされたときの挙動
+    /// </summary>
+    public abstract void Countered();
+}
