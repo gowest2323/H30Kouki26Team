0c7a809fd1ba4b7e32b1a82ae34b4a9185581114
Koya <koya0306@outlook.jp>
2018-10-12 09:39:36 +0900

    CheckPoint, StageManagerを追加 (プルリク来てましたが master から分岐してたので)

---
+++ Kimetu/Assets/Script/StageManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ public class StageManager : MonoBehaviour {
+    private Vector3 restartPosition;
+     public void Pass(Vector3 position)
+    {
+        restartPosition = position;
+    }
+     public Vector3 RestartPosition()
+    {
+        return restartPosition;
+    }
+}
