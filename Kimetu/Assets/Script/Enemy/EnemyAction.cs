c208d3acf2f437cdadc9582de2cf0f6cf65d22e6
KPC-017A1377\K017A1377 <matsumoto.k017a1377@gmail.com>
2018-10-12 11:30:51 +0900

    Update Kimetu/Assets/Script/Enemy/EnemyAction.cs

--- Kimetu/Assets/Script/Enemy/EnemyAction.cs
+++ Kimetu/Assets/Script/Enemy/EnemyAction.cs
@@ -1,22 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAction : MonoBehaviour {
-	private EnemyAnimation enemyAnimation;
+public class EnemyAction : MonoBehaviour, IDamageable
+{
+    private EnemyAnimation enemyAnimation;
+    private Status status;
 
-	// Use this for initialization
-	void Start () {
-		this.enemyAnimation = new EnemyAnimation(GetComponent<Animator>());
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        this.enemyAnimation = new EnemyAnimation(GetComponent<Animator>());
+        status = GetComponent<Status>();
+    }
 
-	public void OnHit(Weapon weapon) {
-		//TODO:ここでダメージアニメーションを開始する
-		//TODO:HPを減らす
-	}
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnHit(DamageSource damageSource)
+    {
+        status.Damage(damageSource.damage);
+        //死亡したら倒れるモーション
+        if (status.IsDead())
+        {
+            //TODO: 倒れるモーションを再生
+            //enemyAnimation.Start...();
+        }
+        //まだ生きていたらダメージモーション
+        else
+        {
+            //enemyAnimation.StartDamageAnimation();
+        }
+    }
 }
