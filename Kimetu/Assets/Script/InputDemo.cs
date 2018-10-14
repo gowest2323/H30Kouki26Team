24fcfd72b8be34083152e9bf5f7fa7ededfcd850
Koya <koya0306@outlook.jp>
2018-10-14 16:14:21 +0900

    UnityTestのためのアセンブリ、フォルダを設定 EditModeでのテスト -> Assets/Tests/EditMode PlayModeでのテスト -> Assets/Tets/PlayMode/Editor

---
+++ Kimetu/Assets/Script/InputDemo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputDemo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton(InputMap.Type.AButton.GetInputName())) {
+			Debug.Log("a button");
+		}
+		if(Input.GetButton(InputMap.Type.BButton.GetInputName())) {
+			Debug.Log("b button");
+		}
+		if(Input.GetButton(InputMap.Type.XButton.GetInputName())) {
+			Debug.Log("x button");
+		}
+		if(Input.GetButton(InputMap.Type.YButton.GetInputName())) {
+			Debug.Log("y button");
+		}
+	}
+}
