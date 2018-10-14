484555fa9c5adc1b8fb399cdd77faf39c379841e
Koya <koya0306@outlook.jp>
2018-10-14 13:19:36 +0900

    AudioNameCreator.cs を追加

---
+++ Kimetu/Assets/Script/Define/AudioName.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// オーディオ名を定数で管理するクラス
+/// </summary>
+public static class AudioName
+{
+	
+	  public const string SE_CLUB = "Club";
+	  public const string SE_CUT = "Cut";
+	  public const string SE_DASH = "Dash";
+	  public const string SE_DODGE = "Dodge";
+	  public const string SE_DRAWING = "Drawing";
+	  public const string SE_HIPDROP = "Hipdrop";
+	  public const string SE_LANCE = "Lance";
+	  public const string SE_LANCE2 = "Lance2";
+	  public const string SE_REPEL = "Repel";
+	  public const string SE_TACKLE = "Tackle";
+	  public const string SE_WALK = "Walk";
+}
