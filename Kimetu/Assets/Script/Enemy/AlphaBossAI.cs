ec2ea68ed03cbe320feaf584af8d34b73276eab7
Koya <koya0306@outlook.jp>
2018-10-15 09:40:55 +0900

    NavMeshComponents を追加 (動的にベイクを焼くために必要)

---
+++ Kimetu/Assets/Script/Enemy/AlphaBossAI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// アルファ版ボスのAI。
+/// </summary>
+public class AlphaBossAI : AI {
+	private GameObject playerObj;
+
+	// Use this for initialization
+	public override void Start () {
+		base.Start();
+		this.playerObj = GameObject.FindWithTag(TagName.Player.String());
+	}
+	
+	// Update is called once per frame
+	public override void Update () {
+		base.Update();
+		var playerPos = playerObj.transform.position;
+		var selfPos = transform.position;
+		var distance = Vector3.Distance(playerPos, selfPos);
+		if(distance > 3) {
+	
+		}
+	}
+}
