e5b3aa2fcd072763cb5202f1ab09b2bb32e7b768
Koya <koya0306@outlook.jp>
2018-10-15 09:55:09 +0900

    - AlphaBossAI を追加 - それに伴ってAI.csの #Start #Update を virtual へ - NavMeshAgentの動的ベイクをテスト - とりあえず追いかけてくるように

--- Kimetu/Assets/Script/Enemy/AlphaBossAI.cs
+++ Kimetu/Assets/Script/Enemy/AlphaBossAI.cs
@@ -1,27 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Assertions;
 
 /// <summary>
 /// アルファ版ボスのAI。
 /// </summary>
 public class AlphaBossAI : AI {
+	[SerializeField]
+	private NavMeshAgent agent;
+
 	private GameObject playerObj;
 
+	private void Awake() {
+		
+	}
 	// Use this for initialization
 	public override void Start () {
 		base.Start();
+		if(agent == null) {
+			this.agent = GetComponent<NavMeshAgent>();
+		}
 		this.playerObj = GameObject.FindWithTag(TagName.Player.String());
+		Assert.IsTrue(agent != null);
+		Assert.IsTrue(playerObj != null);
+		StartCoroutine(Research());
 	}
 	
 	// Update is called once per frame
 	public override void Update () {
 		base.Update();
-		var playerPos = playerObj.transform.position;
-		var selfPos = transform.position;
-		var distance = Vector3.Distance(playerPos, selfPos);
-		if(distance > 3) {
-	
+	}
+
+	private IEnumerator Research() {
+		//1秒ごとに自分と敵の間の距離をみて、
+		//ある程度離れていたなら追跡する
+		var wait = new WaitForSeconds(1f);
+		while(true) {
+			yield return wait;
+			var distance = Vector3.Distance(playerObj.transform.position, transform.position);
+			if(distance > 3) {
+				agent.SetDestination(playerObj.transform.position);
+			}
 		}
 	}
 }
