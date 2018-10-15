bf6a6a4bbcb31daffa2c616235a9ae8248bbc25d
Koya <koya0306@outlook.jp>
2018-10-15 11:23:37 +0900

    ICharacterAnimationProvider.cs を追加

---
+++ Kimetu/Assets/Script/ICharacterAnimationProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// アニメーションをスローにする時、
+/// 現在のシーンのすべてのアニメーションを取得するために使います。
+/// 実際には MonoBehaivour を継承するクラスでこれを実装してください。
+/// </summary>
+public interface ICharacterAnimationProvider {
+	CharacterAnimation characterAnimation { get; }
+}
