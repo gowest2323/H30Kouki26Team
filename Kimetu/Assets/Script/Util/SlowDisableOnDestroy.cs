841ed4a99e9f486370ee365524a958126112554f
Koya <koya0306@outlook.jp>
2018-10-15 11:36:56 +0900

    スロー中に死亡したら自身を"スロー中の一覧"から削除するスクリプトを追加

---
+++ Kimetu/Assets/Script/Util/SlowDisableOnDestroy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// オブジェクトが破棄される前に Slow から自身を削除します。
+/// </summary>
+public class SlowDisableOnDestroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnDestroy() {
+		Slow.Instance.Remove(GetComponent<ICharacterAnimationProvider>().characterAnimation);
+	}
+}
