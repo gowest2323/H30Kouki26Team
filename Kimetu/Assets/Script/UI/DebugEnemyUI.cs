d2f66bea0ba7a1a812639f24201b689c370eaddb
Koya <koya0306@outlook.jp>
2018-10-16 20:50:58 +0900

    DebugEnemyUI を追加

---
+++ Kimetu/Assets/Script/UI/DebugEnemyUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+/// <summary>
+/// EnemyのHPとスライダーの表示を同期します。
+/// </summary>
+public class DebugEnemyUI : MonoBehaviour {
+	[SerializeField]
+	private Slider slider;
+	[SerializeField]
+	private EnemyStatus status;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		slider.value = status.GetHPRatio();
+	}
+}
---
+++ Kimetu/Assets/Script/UI/TrackUI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+/// <summary>
+/// キャンバス, UI, ターゲットを指定して、
+/// UIが常にターゲットと同じ位置にいるようにします。
+/// </summary>
+public class TrackUI : MonoBehaviour {
+	[SerializeField]
+	private RectTransform canvasRect;
+
+	[SerializeField]
+	private RectTransform uiRect;
+
+	[SerializeField]
+	private GameObject target;
+	public bool track {
+		set; get;
+	}
+
+	// Use this for initialization
+	void Start () {
+		Assert.IsTrue(uiRect != null);
+		Assert.IsTrue(target != null);
+		if(this.canvasRect == null) {
+			this.canvasRect = GetComponent<RectTransform>();
+		}
+		Assert.IsTrue(canvasRect != null);
+		this.track = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!track) { return; }
+		Vector2 ViewportPosition=Camera.main.WorldToViewportPoint(target.transform.position);
+		Vector2 WorldObject_ScreenPosition=new Vector2(
+		((ViewportPosition.x*canvasRect.sizeDelta.x)-(canvasRect.sizeDelta.x*0.5f)),
+		((ViewportPosition.y*canvasRect.sizeDelta.y)-(canvasRect.sizeDelta.y*0.5f)));
+
+		//now you can set the position of the ui element
+		uiRect.anchoredPosition=WorldObject_ScreenPosition;
+	}
+}
