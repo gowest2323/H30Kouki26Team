ff11f01c761ea8e2e6289edb3a3806c064228224
satoryo3163510 <43367124+satoryo3163510@users.noreply.github.com>
2018-10-19 13:05:27 +0900

    ダッシュ実装

--- Kimetu/Assets/Script/Player/PlayerController.cs
+++ Kimetu/Assets/Script/Player/PlayerController.cs
@@ -4,20 +4,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 	private PlayerAction action;
+    int pressButton = 0, holdShort = 5, holdLong = 15;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		this.action = GetComponent<PlayerAction>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        pressButton += (Input.GetButton(InputMap.Type.AButton.GetInputName())) ? 1 : 0;
+
+        if (Input.GetButton(InputMap.Type.AButton.GetInputName()))
+        {
+            if (holdLong <= pressButton)
+            {
+                Dash();
+                Debug.Log("長押し");
+            }
+            else if (holdShort <= pressButton)
+            {
+                Avoid();
+                Debug.Log("押し");
+            }
+        }
+
+        if (Input.GetButtonUp(InputMap.Type.AButton.GetInputName())) pressButton = 0;
+        //if (pressButton > 20) pressButton = 0;
+        Debug.Log(pressButton);
+
         if (!action.IsAvoid())
         {
             Move();
             Attack();
         }
-        Avoid();
+        //Avoid();
     }
 
 	private void Move() {
@@ -27,10 +49,24 @@ public class PlayerController : MonoBehaviour {
 			Input.GetAxis(InputMap.Type.LStick_Vertical.GetInputName())
 		);
 		action.Move(dir);
-	}
 
-	private void Attack() {
-		if(Input.GetKey(KeyCode.Z)) {
+        //if (Input.GetButton("WIN_AButton"))
+        //{
+        //    action.Move(dir * 1.05f);
+        //}
+    }
+
+    private void Dash()
+    {
+        var dir = new Vector3(
+            Input.GetAxis(InputMap.Type.LStick_Horizontal.GetInputName()),
+            0,
+            Input.GetAxis(InputMap.Type.LStick_Vertical.GetInputName())
+        );
+        action.Move(dir * 1.05f);
+    }
+    private void Attack() {
+		if(Input.GetKey(KeyCode.Z)||Input.GetButton("WIN_XButton")) {
 			action.Attack();
 		}
 	}
@@ -42,10 +78,6 @@ public class PlayerController : MonoBehaviour {
             0,
             Input.GetAxis("Vertical")
         );
-
-        if (Input.GetKeyDown(KeyCode.X))
-        {
-            action.Avoid(dir);
-        }
+        action.Avoid(dir);
     }
 }
