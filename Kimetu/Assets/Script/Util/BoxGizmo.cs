f2da046da6af9fba839787a098620c0492e93db6
Koya <koya0306@outlook.jp>
2018-11-02 09:38:08 +0900

    デバッグ用のスクリプト追加 エネミースポナーの位置などを視覚的に確認できるように

---
+++ Kimetu/Assets/Script/Util/BoxGizmo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxGizmo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnDrawGizmos() {
+		var color = Gizmos.color;
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawCube(transform.position, transform.localScale);
+		Gizmos.color = color;
+	}
+}
