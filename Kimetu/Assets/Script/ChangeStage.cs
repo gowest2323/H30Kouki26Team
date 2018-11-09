7dd0685bc28fa4f3dacb53b624a7615e4c002630
satoryo3163510 <43367124+satoryo3163510@users.noreply.github.com>
2018-11-09 11:17:38 +0900

    ステージ遷移用のオブジェクト作成

---
+++ Kimetu/Assets/Script/ChangeStage.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeStage : MonoBehaviour {
+    private bool playerStay;
+    public GameObject canvas;
+
+	// Use this for initialization
+	void Start () {
+        playerStay = false;
+        canvas.gameObject.SetActive(false);
+        GetComponent<LongPressDetector>();
+        GetComponent<LongPressDetector>().OnLongPressEnd += () => {
+            if (playerStay == true)
+            {
+                Debug.Log("ステージ移行できます");
+            }
+        };
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerStay = true;
+            canvas.gameObject.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playerStay = false;
+            canvas.gameObject.SetActive(false);
+        }
+    }
+}
