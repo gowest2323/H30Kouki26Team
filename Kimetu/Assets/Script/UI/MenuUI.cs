48d8d0d0008d5ee5bcbe29d85d1784f4f648773c
Koya <koya0306@outlook.jp>
2018-11-12 10:01:55 +0900

    タイトルのメニュー移動実装

---
+++ Kimetu/Assets/Script/UI/MenuUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuUI : MonoBehaviour {
+	[SerializeField]
+	private Text[] menuItems;
+
+	private int selected;
+	private float time;
+
+	// Use this for initialization
+	void Start () {
+		time = -2;
+		Select(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis(InputMap.Type.LStick_Vertical.GetInputName()) >= 0.9f) {
+			Select(this.selected - 1);
+		} else if(Input.GetAxis(InputMap.Type.LStick_Vertical.GetInputName()) <= -0.9f) {
+			Select(this.selected + 1);
+		}
+	}
+
+	private void Select(int index) {
+		if(Time.time - time < 0.2f) { return; }
+		if(index < 0) { index = menuItems.Length - 1; }
+		if(index >= menuItems.Length) { index = 0;}
+		foreach(var item in menuItems) {
+			item.color = Color.black;
+		}
+		menuItems[index].color = Color.red;
+		this.selected = index;
+		this.time = Time.time;
+	}
+}
