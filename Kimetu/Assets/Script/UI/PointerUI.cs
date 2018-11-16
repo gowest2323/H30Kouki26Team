41d33f61de2072b6208fc9d66c6099bf54c4d9e6
Koya <koya0306@outlook.jp>
2018-11-16 16:06:32 +0900

    PointerUI追加 敵をロックオンした時のマーカー

---
+++ Kimetu/Assets/Script/UI/PointerUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Reflection;
+
+public class PointerUI : MonoBehaviour {
+	[SerializeField]
+	private CameraController cameraController;
+	[SerializeField]
+	private Image image;
+	private FieldInfo nearEnemyInfo;
+
+
+	// Use this for initialization
+	void Start () {
+		if(this.image == null) {
+			this.image = GetComponentInChildren<Image>();
+		}
+		if(this.cameraController == null) {
+			this.cameraController = Camera.main.gameObject.GetComponentInChildren<CameraController>();
+		}
+		image.color = image.color * new Vector4(1, 1, 1, 0);
+		this.nearEnemyInfo = typeof(CameraController).GetField("nearObj", BindingFlags.NonPublic | BindingFlags.Instance);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var obj = nearEnemyInfo.GetValue(cameraController);
+		var color = image.color;
+		color.a = (cameraController.IsLockOn() ? 1 : 0);
+		image.color = color;
+	}
+}
