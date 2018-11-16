5660ec86e0e7fb8e5d54186aeec641f093788a6a
Koya <koya0306@outlook.jp>
2018-11-16 22:44:32 +0900

    PointerTarget の追加

--- Kimetu/Assets/Script/UI/PointerUI.cs
+++ Kimetu/Assets/Script/UI/PointerUI.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Reflection;
 
+/// <summary>
+/// ロックオン対象にマーカーをつけるスクリプト。
+/// </summary>
 public class PointerUI : MonoBehaviour {
 	[SerializeField]
 	private CameraController cameraController;
@@ -30,5 +33,10 @@ public class PointerUI : MonoBehaviour {
 		var color = image.color;
 		color.a = (cameraController.IsLockOn() ? 1 : 0);
 		image.color = color;
+
+		if(obj != null) {
+			var pos = Camera.main.WorldToScreenPoint(((GameObject)obj).transform.position + Vector3.up);
+			image.transform.position = pos;
+		}
 	}
 }
