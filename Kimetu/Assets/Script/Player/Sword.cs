3740cba089bc8ecdbd905edc207a1b4f0660b8c2
Koya <koya0306@outlook.jp>
2018-11-19 14:31:34 +0900

    プレイヤーの攻撃が敵に対して複数回ヒットしないように

--- Kimetu/Assets/Script/Player/Sword.cs
+++ Kimetu/Assets/Script/Player/Sword.cs
@@ -7,10 +7,12 @@ public class Sword : Weapon
 {
     private int attackNum; //現在の攻撃回数
     private PlayerAnimation playerAnimation; //プレイヤーのアニメーション管理
+    private Dictionary<GameObject, int> countDict;
 
     protected override void Start()
     {
         base.Start();
+        this.countDict = new Dictionary<GameObject, int>();
     }
 
     /// <summary>
@@ -26,6 +28,7 @@ public class Sword : Weapon
     /// </summary>
     public override void AttackStart()
     {
+        countDict.Clear();
         attackNum++;
         weaponCollider.enabled = true;
         //playerAnimation.StartAttackAnimation();
@@ -45,6 +48,12 @@ public class Sword : Weapon
         //敵に当たったら通知する
         if (TagNameManager.Equals(other.tag, TagName.Enemy))
         {
+            //攻撃が複数回ヒットしないように
+            if(!countDict.ContainsKey(other.gameObject)) {
+                countDict[other.gameObject] = 1;
+            } else if(countDict[other.gameObject] >= 1) {
+                return;
+            }
             //衝突したときの最近点を衝突点とする
             Vector3 hitPos = other.ClosestPointOnBounds(this.transform.position);
             DamageSource damage = new DamageSource(hitPos, power, holderObjectDamagable);
