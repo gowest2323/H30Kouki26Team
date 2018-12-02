4b284ebef4210eacb1bec2da9c16b69565284f1a
Koya <koya0306@outlook.jp>
2018-12-03 08:54:30 +0900

    フォーマット微修正

--- Kimetu/Assets/Editor/CodeFormat.cs
+++ Kimetu/Assets/Editor/CodeFormat.cs
@@ -12,19 +12,17 @@ using System.Threading;
 /// Astyleについて(https://qiita.com/hakuta/items/29c988181d40829b1679)
 /// ソースコードのフォーマットを統一するために追加しました。
 /// </summary>
-public class CodeFormat : EditorWindow
-{
+public class CodeFormat : EditorWindow {
 	private List<string> files;
 	private Vector2 scrollPos = Vector2.zero;
 	private string searchText = "";
 	private string astylePath;
 
 	private static readonly string ASTYLE_PATH_KEY = "Kimetu.AstylePath";
-	private static readonly string FORMAT_SETTING = Application.dataPath + "/csfmt.txt";
+	private string FORMAT_SETTING;
 
 	[MenuItem("Editor/CodeFormat")]
-	static void CreateWindow()
-	{
+	static void CreateWindow() {
 		CodeFormat window = (CodeFormat)EditorWindow.GetWindow(typeof(CodeFormat));
 		window.Init();
 		window.Show();
@@ -33,8 +31,8 @@ public class CodeFormat : EditorWindow
 	/// <summary>
 	/// フォーマット対象となるファイルの一覧を取得します。
 	/// </summary>
-	private void Init()
-	{
+	private void Init() {
+		this.FORMAT_SETTING = Application.dataPath + "/csfmt.txt";
 		this.files = new List<string>();
 		this.astylePath = PlayerPrefs.GetString(ASTYLE_PATH_KEY, "astyle");
 		var assets = Application.dataPath;
@@ -47,15 +45,12 @@ public class CodeFormat : EditorWindow
 	/// <summary>
 	/// ファイルをフォーマットするためのGUIを描画。
 	/// </summary>
-	void OnGUI()
-	{
-		if (EditorApplication.isCompiling || Application.isPlaying)
-		{
+	void OnGUI() {
+		if (EditorApplication.isCompiling || Application.isPlaying) {
 			return;
 		}
 
-		if (!File.Exists(FORMAT_SETTING))
-		{
+		if (!File.Exists(FORMAT_SETTING)) {
 			EditorGUILayout.LabelField(string.Format("{0}: not found"), FORMAT_SETTING);
 			return;
 		}
@@ -63,17 +58,17 @@ public class CodeFormat : EditorWindow
 		this.scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
 		EditorGUILayout.BeginVertical();
 
-		if (GUILayout.Button("Update List"))
-		{
+		if (GUILayout.Button("Update List")) {
 			Init();
 			return;
 		}
-		if (GUILayout.Button("Format All"))
-		{
+
+		if (GUILayout.Button("Format All")) {
 			FormatAll();
 			Close();
 			return;
 		}
+
 		ShowExecutableFileBar();
 		ShowSearchBar();
 		ShowFileList();
@@ -83,14 +78,16 @@ public class CodeFormat : EditorWindow
 
 	private void FormatAll() {
 		bool result = EditorUtility.DisplayDialog(
-			"- CodeFormat -",
-			"全てのファイルをフォーマットします。\nよろしいですか？",
-			"OK",
-			"取消し"
-		);
-		if(!result) {
+						  "- CodeFormat -",
+						  "全てのファイルをフォーマットします。\nよろしいですか？",
+						  "OK",
+						  "取消し"
+					  );
+
+		if (!result) {
 			return;
 		}
+
 		files.ForEach((e) => {
 			RunFormat(e);
 		});
@@ -100,8 +97,7 @@ public class CodeFormat : EditorWindow
 	/// <summary>
 	/// 検索バーを描画。
 	/// </summary>
-	private void ShowSearchBar()
-	{
+	private void ShowSearchBar() {
 		EditorGUILayout.BeginHorizontal();
 		EditorGUILayout.LabelField("search:");
 		this.searchText = EditorGUILayout.TextField(searchText);
@@ -111,39 +107,36 @@ public class CodeFormat : EditorWindow
 	/// <summary>
 	/// Astyleの実行ファイルへのパスを設定するUI。
 	/// </summary>
-	private void ShowExecutableFileBar()
-	{
+	private void ShowExecutableFileBar() {
 		EditorGUILayout.BeginHorizontal();
 		EditorGUILayout.LabelField("astyle path:");
 		var temp = astylePath;
 		//値が変更されていたので更新
 		this.astylePath = EditorGUILayout.TextField(astylePath);
-		if(temp != astylePath) {
+
+		if (temp != astylePath) {
 			PlayerPrefs.SetString(ASTYLE_PATH_KEY, astylePath);
 			PlayerPrefs.Save();
 		}
+
 		EditorGUILayout.EndHorizontal();
 	}
 
 	/// <summary>
 	/// フォーマット対象一覧の表示。
 	/// </summary>
-	private void ShowFileList()
-	{
-		foreach (var filename in files)
-		{
+	private void ShowFileList() {
+		foreach (var filename in files) {
 			var pathname = Path.GetFileName(filename);
 
-			if (searchText.Length > 0 && !pathname.Contains(searchText))
-			{
+			if (searchText.Length > 0 && !pathname.Contains(searchText)) {
 				continue;
 			}
 
 			EditorGUILayout.BeginHorizontal();
 			EditorGUILayout.LabelField(pathname);
 
-			if (GUILayout.Button("Format"))
-			{
+			if (GUILayout.Button("Format")) {
 				RunFormat(filename);
 			}
 
@@ -151,30 +144,25 @@ public class CodeFormat : EditorWindow
 		}
 	}
 
-	private void RunFormat(string filename)
-	{
-		try
-		{
+	private void RunFormat(string filename) {
+		try {
 			var cmd = string.Format(astylePath + " --options={0} {1}", FORMAT_SETTING, filename);
 			UnityEngine.Debug.Log(cmd);
 			DoCrossPlatformCommand(cmd);
-		}
-		catch (System.Exception e)
-		{
+		} catch (System.Exception e) {
 			UnityEngine.Debug.LogError(e);
 		}
 	}
 
 	static void DoCrossPlatformCommand(string cmd) {
 		#if UNITY_STANDALONE_OSX
-			DoBashCommand(cmd);
+		DoBashCommand(cmd);
 		#else
-			DoDOSCommand(cmd);
+		DoDOSCommand(cmd);
 		#endif
 	}
 
-	static void DoBashCommand(string cmd)
-	{
+	static void DoBashCommand(string cmd) {
 		var p = new Process();
 		p.StartInfo.FileName = "/bin/bash";
 		p.StartInfo.Arguments = "-c \" " + cmd + " \"";
@@ -189,8 +177,7 @@ public class CodeFormat : EditorWindow
 		LogResult(output, error);
 	}
 
-	static void DoDOSCommand(string cmd)
-	{
+	static void DoDOSCommand(string cmd) {
 		var p = new Process();
 		//p.StartInfo.FileName = "cmd.exe";
 		p.StartInfo.FileName = System.Environment.GetEnvironmentVariable("ComSpec");
@@ -207,10 +194,11 @@ public class CodeFormat : EditorWindow
 	}
 
 	private static void LogResult(string output, string error) {
-		if(output.Length > 0) {
+		if (output.Length > 0) {
 			UnityEngine.Debug.Log(output);
 		}
-		if(error.Length > 0) {
+
+		if (error.Length > 0) {
 			UnityEngine.Debug.LogError(error);
 		}
 	}
