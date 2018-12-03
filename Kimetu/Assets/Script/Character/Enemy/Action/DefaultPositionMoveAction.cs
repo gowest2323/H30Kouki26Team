6ef9c2f56a3e3cbeccd7dc701bc8ad61b8dc31cd
matsumoto <matsumoto.k017a1377@gmail.com>
2018-12-03 16:03:02 +0900

    プレイヤーを見失った時にもとの場所に戻るように変更

---
+++ Kimetu/Assets/Script/Character/Enemy/Action/DefaultPositionMoveAction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultPositionMoveAction : MoveAction
+{
+    protected override void Start()
+    {
+        base.Start();
+        movePosition = this.transform.position;
+        moveRotation = this.transform.rotation;
+    }
+}
--- Kimetu/Assets/Script/Character/Enemy/EnemyState.cs
+++ Kimetu/Assets/Script/Character/Enemy/EnemyState.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 //NOTE:Enemyの種類ごとに XXXState を用意する必要があるかもしれません
 public enum EnemyState {
 	Idle, //待機　
-	Move, //移動注
+	Move, //移動中
 	Attack, //攻撃時
 	Search, //索敵時
 	Damage, //被弾時
 	MoveNear, //接近中
+    MoveDefaultPosition, //デフォルトの場所に移動中
 	Death, //倒れている
 	None,
 }
