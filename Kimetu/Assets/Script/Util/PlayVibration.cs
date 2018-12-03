f0f82b6a5c3ed1d1fae360ba7c7b020ef15dbec3
VANO0125 <43366986+VANO0125@users.noreply.github.com>
2018-12-03 16:09:36 +0900

    バイブレーション機能の追加
    PlayVibration.Instance.StartVibration(何秒間バイブレーションさせるか);
    で利用可能

---
+++ Kimetu/Assets/Script/Util/PlayVibration.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVibration : SingletonMonoBehaviour<PlayVibration> {
+
+	public void StartVibration(float time) {
+		StartCoroutine(Play(time));
+	}
+
+	/// <summary>
+	/// バイブレーション機能
+	/// </summary>
+	/// <param name="vibrationTime">何秒間バイブレーションするか</param>
+	/// <returns></returns>
+	private IEnumerator Play(float vibrationTime)
+
+	{
+		XInputDotNetPure.GamePad.SetVibration(0, 1.0f, 1.0f);
+		yield return new WaitForSeconds(vibrationTime);
+		XInputDotNetPure.GamePad.SetVibration(0, 0.0f, 0.0f);
+
+	}
+
+}
