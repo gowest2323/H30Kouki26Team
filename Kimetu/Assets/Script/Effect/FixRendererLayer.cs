8be47d1208269371418c0f3af999379d46cd4bd8
koya <gnndr36@gmail.com>
2018-12-10 10:21:48 +0900

    実行時にレンダラーの設定を変更するスクリプトを追加

---
+++ Kimetu/Assets/Script/Effect/FixRendererLayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// レンダラーの設定を実行時に変更します。
+/// </summary>
+public class FixRendererLayer : MonoBehaviour {
+	[SerializeField]
+	private Renderer target;
+
+	[SerializeField]
+	private SortingLayer sortingLayer;
+
+	[SerializeField]
+	private int orderInLayer = 0;
+
+	// Use this for initialization
+	void Start () {
+		if (target == null) {
+			this.target = GetComponent<Renderer>();
+		}
+
+		target.sortingLayerName = sortingLayer.name;
+		target.sortingOrder = orderInLayer;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
