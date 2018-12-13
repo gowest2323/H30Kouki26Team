2616f0a279c383baa97effdf1fa1882970e7a815
Koya <koya0306@outlook.jp>
2018-12-13 21:42:51 +0900

    ReplEffectGeneratorを追加

---
+++ Kimetu/Assets/Script/Effect/ReplEffectGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 弾きエフェクトを生成するためのクラス。
+/// </summary>
+public class ReplEffectGenerator : MonoBehaviour {
+	[SerializeField]
+	private int repeatCount = 1;
+
+	[SerializeField]
+	private float generateWait = 1.5f;
+
+	[SerializeField]
+	private GameObject particlePrefab;
+
+	private bool generating;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		#if UNITY_EDITOR
+		if(Input.GetKeyDown(KeyCode.E)) {
+			var player = GameObject.FindGameObjectWithTag("Player");
+			StartGenerate(player.transform.position + new Vector3(0, 2, 0));
+		}
+		#endif
+	}
+
+	/// <summary>
+	/// エフェクトの生成を開始します。
+	/// </summary>
+	/// <param name="position"></param>
+	public void StartGenerate(Vector3 position) {
+		if(generating) {
+			return;
+		}
+		StartCoroutine(GenerateUpdate(position));
+	}
+
+	private IEnumerator GenerateUpdate(Vector3 position) {
+		this.generating = true;
+		for(int i=0; i<repeatCount; i++) {
+			var obj = GameObject.Instantiate(particlePrefab);
+			var particleSystem = obj.GetComponent<ParticleSystem>();
+			obj.transform.parent = transform;
+			obj.transform.position = position;
+			//パーティクルの1ループの長さだけ待機
+			GameObject.Destroy(obj, particleSystem.main.duration);
+			yield return new WaitForSeconds(generateWait);
+		}
+		this.generating = false;
+	}
+}
