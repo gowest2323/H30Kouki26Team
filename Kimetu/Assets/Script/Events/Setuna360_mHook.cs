a03765943a606f93f911d8dcbea045c656349578
matsumoto <matsumoto.k017a1377@gmail.com>
2018-12-17 10:12:00 +0900

    ラスボスのひな型作成

---
+++ Kimetu/Assets/Script/Events/Setuna360_mHook.cs
@@ -0,0 +1,37 @@
+﻿//AUTO-GENERATED-CODE
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UniRx;
+
+[DisallowMultipleComponent]
+public class Setuna360_mHook : MonoBehaviour, IEventHook {
+	public bool animationNow { private set; get; }
+	public IObservable<bool> trigger { get { return subject; }}
+	private Subject<bool> subject;
+
+	void Awake() {
+		this.subject = new Subject<bool>();
+	}
+
+	void Start() {
+	}
+
+	public void Setuna360_mStart() {
+		Debug.Log("Setuna360_mStart");
+		this.animationNow = true;
+		subject.OnNext(true);
+	}
+
+	public void Setuna360_mEnd() {
+		Debug.Log("Setuna360_mEnd");
+		this.animationNow = false;
+		subject.OnNext(false);
+	}
+
+	public IEnumerator Wait() {
+		yield return new WaitWhile(() => !animationNow);
+		yield return new WaitWhile(() => animationNow);
+	}
+}
