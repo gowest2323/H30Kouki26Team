90eb6a258ad8c90e7c52d89c7c2bc2386d8063c6
Koya <koya0306@outlook.jp>
2018-12-27 18:21:32 +0900

    切り抜けで円移動していたのを直線移動へ変更

--- Kimetu/Assets/Script/Character/Player/Kirinuke.cs
+++ Kimetu/Assets/Script/Character/Player/Kirinuke.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Kirinuke : MonoBehaviour {
-	[SerializeField, Header("近くのにかかる時間")]
+	[SerializeField, Header("近付くのにかかる時間")]
 	private float moveSeconds = 0.5f;
-	[SerializeField, Header("回りこむのにかかる時間")]
-	private float turnSeconds = 1f;
 
-	[SerializeField, Header("敵の向こう側へ行く時、どれだけ距離を空けるか")]
-	private float moveDistanceLimit = 2f;
+	[SerializeField, Header("プレイヤーから敵へのベクトルをどれだけずらすか"), Range(0f, 360f)]
+	private float adjustDegree = 45f;
 
-	[SerializeField, Header("回りこむ角度"), Range(0f, 360f)]
-	private float turnDegree = 100f;
+	[SerializeField, Header("敵をどれだけ通り過ぎるか")]
+	private float distanceToEnemy = 1.5f;
 
 	[SerializeField]
 	private PlayerAnimation playerAnimation;
@@ -63,7 +61,7 @@ public class Kirinuke : MonoBehaviour {
 			playerAnimation.StartKirinukeAnimation();
 			this.target = enemy;
 			yield return MoveToEnemy();
-			yield return TurnToEnemyBack();
+			//yield return TurnToEnemyBack();
 			playerAnimation.StopKirinukeAnimation();
 			this.isRunning = false;
 		} else {
@@ -72,11 +70,8 @@ public class Kirinuke : MonoBehaviour {
 	}
 
 	private IEnumerator MoveToEnemy() {
-		var dir = (target.transform.position - transform.position).normalized;
-		var dist = Utilities.DistanceXZ(target.transform.position, transform.position) - moveDistanceLimit;
-		if(dist < 0) {
-			yield break;
-		}
+		var dir = Quaternion.Euler(0, adjustDegree, 0) * (target.transform.position - transform.position).normalized;
+		var dist = Utilities.DistanceXZ(target.transform.position, transform.position) + distanceToEnemy;
 		var start = transform.position;
 		var end = start + (dir * dist);
 		var offset = 0f;
@@ -88,34 +83,7 @@ public class Kirinuke : MonoBehaviour {
 			var parcent = offset / moveSeconds;
 			transform.position = start + (dir * dist * parcent);
 			var currDistance = Utilities.DistanceXZ(transform.position, target.transform.position);
-			if(currDistance < moveDistanceLimit) {
-				break;
-			}
 		}
 		transform.position = end;
-	}
-
-	private IEnumerator TurnToEnemyBack() {
-		//円の中心をプレイヤーとする
-		var center = target.transform.position;
-		//自分からプレイヤーへの線が難度であるかをここで取得する
-		var dirToCenter =center- transform.position;
-		var radian = Mathf.Atan2(dirToCenter.z, dirToCenter.x) + (180f * Mathf.Deg2Rad);
-		//一定時間で補完する
-		var start = transform.position;
-		var offset = 0f;
-		while(offset < turnSeconds) {
-			var t = Time.time;
-			yield return null;
-			var diff = (Time.time - t) * Slow.Instance.GetPlayerSpeed();
-			offset += diff;
-			//最初にいた時点での角度 + 移動する角度
-			var parcent = Mathf.Clamp01(offset / turnSeconds);
-			var dirx = Mathf.Cos(radian + ((turnDegree * parcent) * Mathf.Deg2Rad));
-			var dirz = Mathf.Sin(radian + ((turnDegree * parcent) * Mathf.Deg2Rad));
-			var dirv = new Vector3(dirx, 0, dirz);
-			transform.LookAt(target.transform.position);
-			transform.position = center + (dirv * moveDistanceLimit);
-		}
 	}
 }
