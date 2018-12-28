5415ce8491ce3320c5df0e4d74f6cf115deaaa61
Koya <koya0306@outlook.jp>
2018-12-28 21:01:05 +0900

    Rengeki関連のクラス追加

---
+++ Kimetu/Assets/Script/Character/Player/Rengeki.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class RengekiPushEvent {
+	public int pushMaxCount { private set; get; }
+	public int pushCurrentCount { private set; get; }
+	public float parcent { get { return ((float)pushCurrentCount / (float)pushMaxCount); } }
+
+	public RengekiPushEvent(int pushMaxCount, int pushCurrentCount) {
+		this.pushMaxCount = pushMaxCount;
+		this.pushCurrentCount = pushCurrentCount;
+	}
+}
+
+public class Rengeki : MonoBehaviour {
+	[SerializeField]
+	private int pushMaxCount = 20;
+
+	private bool triggered;
+	private int pushCurrentCount;
+
+	public IObservable<RengekiPushEvent> onPush { get { return push; } }
+	private Subject<RengekiPushEvent> push;
+
+	private System.IDisposable startObserver;
+	private System.IDisposable endObserver;
+
+	// Use this for initialization
+	void Start () {
+		this.push = new Subject<RengekiPushEvent>();
+		this.startObserver = Slow.Instance.onStart.Subscribe(OnSlowStart);
+		this.endObserver = Slow.Instance.onEnd.Subscribe(OnSlowStart);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!Slow.Instance.isSlowNow) {
+			return;
+		}
+		//押してない || もう発動中
+		if(!Input.GetButtonDown(InputMap.Type.XButton.GetInputName()) ||
+			pushCurrentCount >= pushMaxCount) {
+			return;
+		}
+		pushCurrentCount++;
+		push.OnNext(new RengekiPushEvent(pushMaxCount, pushCurrentCount));
+		if(pushCurrentCount >= pushMaxCount) {
+			Debug.Log("rengeki");
+		}
+	}
+
+	private void OnDestroy() {
+		startObserver.Dispose();
+		endObserver.Dispose();
+	}
+
+	private void OnSlowStart(bool b) {
+		this.pushCurrentCount = 0;
+	}
+
+	private void OnSlowEnd(bool b) {
+
+	}
+}
---
+++ Kimetu/Assets/Script/UI/RengekiUI.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UnityEngine.UI;
+
+public class RengekiUI : MonoBehaviour {
+	[SerializeField]
+	private GameObject root;
+
+	[SerializeField]
+	private Slider pushSlider;
+
+	[SerializeField]
+	private Rengeki rengeki;
+
+	private System.IDisposable startObserver;
+	private System.IDisposable endObserver;
+
+	// Use this for initialization
+	void Start () {
+		if(root == null) {
+			this.root = transform.FindRec("LBack");
+		}
+		if(pushSlider == null) {
+			this.pushSlider = root.transform.FindRec("LSlider").GetComponent<Slider>();
+		}
+		if (rengeki == null) {
+			this.rengeki = GameObject.FindGameObjectWithTag(TagName.Player.String()).GetComponent<Rengeki>();
+		}
+		root.SetActive(false);
+		rengeki.onPush.Subscribe((e) => {
+			pushSlider.value = e.parcent;
+		});
+		this.startObserver = Slow.Instance.onStart.Subscribe(OnSlowStart);
+		this.endObserver = Slow.Instance.onEnd.Subscribe(OnSlowEnd);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnDestroy() {
+		startObserver.Dispose();
+		endObserver.Dispose();
+	}
+
+	private void OnSlowStart(bool b) {
+		root.SetActive(true);
+	}
+
+	private void OnSlowEnd(bool b) {
+		root.SetActive(false);
+	}
+}
