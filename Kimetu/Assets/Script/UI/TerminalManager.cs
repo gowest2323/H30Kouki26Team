e0bacf3bda4fe19723f7d07d9b5663ea92ca04a5
Koya <koya0306@outlook.jp>
2018-12-28 22:55:05 +0900

    デバッグ用のターミナルを追加

---
+++ Kimetu/Assets/Script/UI/TerminalManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerminalManager : MonoBehaviour {
+	[SerializeField]
+	private GameObject terminalPrefab;
+
+	// Use this for initialization
+	void Start () {
+#if UNITY_EDITOR
+		GameObject.Instantiate(terminalPrefab);
+#endif
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Kimetu/Assets/Script/UI/TerminalUI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TerminalUI : MonoBehaviour {
+	[SerializeField]
+	private Image root;
+
+	[SerializeField]
+	private Text header;
+
+	[SerializeField]
+	private Text editor;
+
+	private bool show;
+	private string defaultHeaderText, defaultEditorText;
+
+	// Use this for initialization
+	void Start () {
+		this.defaultHeaderText = header.text;
+		this.defaultEditorText = editor.text;
+		ToggleEditor(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Slash)) {
+			ToggleEditor();
+		}
+		if (!show) {
+			return;
+		}
+	}
+
+	private void ToggleEditor() {
+		ToggleEditor(!show);
+	}
+
+	private void ToggleEditor(bool enable) {
+		if(enable) {
+			this.header.text = defaultHeaderText;
+			this.editor.text = defaultEditorText;
+		}
+		root.gameObject.SetActive(enable);
+		this.show = enable;
+	}
+}
