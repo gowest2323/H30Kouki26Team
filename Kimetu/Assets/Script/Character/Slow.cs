c0bee40695f977e400b9e996eb1e32b75b8bc5f2
Koya <koya0306@outlook.jp>
2018-12-30 11:22:49 +0900

    Slow#GetWaitSeconds を追加

--- Kimetu/Assets/Script/Character/Slow.cs
+++ Kimetu/Assets/Script/Character/Slow.cs
@@ -112,6 +112,7 @@ public class Slow : SingletonMonoBehaviour<Slow> {
 		slowAnimationList.RemoveAll(anim => anim == animation);
 	}
 
-
-
+	public float GetWaitSeconds() {
+		return slowTime;
+	}
 }
