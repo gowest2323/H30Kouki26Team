1d408e7a35da1401b91e69fceb8928029ff2c161
Koya <koya0306@outlook.jp>
2019-01-07 20:46:38 +0900

    歩く部分を実装

---
+++ Kimetu/Assets/Script/Movie/RotateToPart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateToPart : MoviePart {
+	[SerializeField]
+	private GameObject target;
+	[SerializeField]
+	private GameObject point;
+	[SerializeField]
+	private float seconds = 1.0f;
+	//FIXME:CameraRotateToのコピペ
+	public override IEnumerator MovieUpdate() {
+		var selfPos = target.transform.position;
+		var targetPos = point.transform.position;
+		targetPos.y = selfPos.y;
+		var dir = (targetPos - selfPos).normalized;
+		var startRotation = target.transform.rotation;
+		var endRotation = Quaternion.LookRotation(dir);
+		var offset = 0f;
+		while (offset < seconds) {
+			var t = Time.time;
+			yield return null;
+			offset += (Time.time - t);
+			target.transform.rotation = Quaternion.Slerp(startRotation, endRotation, offset / seconds);
+		}
+	}
+}
---
+++ Kimetu/Assets/Script/Movie/WalkPart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkPart : MoviePart {
+	[SerializeField]
+	private GameObject target;
+
+	[SerializeField]
+	private GameObject walkTo;
+
+	[SerializeField]
+	private float seconds = 3f;
+
+	public override IEnumerator MovieUpdate() {
+		var enemyAnimation = target.GetComponent<EnemyAnimation>();
+		var offset = 0f;
+		var start = target.transform.position;
+		var end = walkTo.transform.position;
+		enemyAnimation.StartRunAnimation();
+		while(offset < seconds) {
+			var t = Time.time;
+			yield return null;
+			offset += (Time.time - t);
+			target.transform.position = Vector3.Lerp(start, end, offset / seconds);
+		}
+		target.transform.position = end;
+		enemyAnimation.StopRunAnimation();
+	}
+}
