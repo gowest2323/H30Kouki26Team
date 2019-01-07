c021d5d3f4c3180303b80b78bc6d362f9988b2c2
satoryo3163510 <43367124+satoryo3163510@users.noreply.github.com>
2019-01-07 15:28:07 +0900

    プレイヤーの回復エフェクト変更

--- Kimetu/Assets/Script/Effect/PlayerHealEffectMove.cs
+++ Kimetu/Assets/Script/Effect/PlayerHealEffectMove.cs
@@ -5,36 +5,17 @@ using UnityEngine;
 public class PlayerHealEffectMove : MonoBehaviour
 {
     ParticleSystem particle;
-    ParticleSystem.Particle[] particles = new ParticleSystem.Particle[5000];
-    public GameObject target_p;
-    bool healstart;
-    PlayerAction action;
 
     // Use this for initialization
     void Start()
     {
         particle = GetComponent<ParticleSystem>();
-        healstart = false;
-        action = new PlayerAction();
     }
-
     // Update is called once per frame
     void Update()
     {
         Transform target = particle.GetComponentInParent<Transform>();
         Vector3 axis = target.transform.TransformDirection(Vector3.forward);
-        particle.transform.RotateAround(target.position, axis, 7.0f);
-
-        int count = particle.GetParticles(particles);
-        for (int n = 0; n < particles.Length; n++)
-        {
-            float remainTime = particles[n].remainingLifetime;
-            if (action.CanPierce() == true)
-            {
-                particles[n].position = Vector3.Lerp(particles[n].position, target_p.transform.position, 1f - remainTime);
-            }
-            healstart = true;
-        }
-        particle.SetParticles(particles, count);
+        particle.transform.RotateAround(target.position, axis, 5.0f);
     }
 }
