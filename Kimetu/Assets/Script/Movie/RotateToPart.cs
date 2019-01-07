79a2265ac830ec367f142f7833a373f4aea610ac
Koya <koya0306@outlook.jp>
2019-01-07 21:57:18 +0900

    ムービー更新

--- Kimetu/Assets/Script/Movie/RotateToPart.cs
+++ Kimetu/Assets/Script/Movie/RotateToPart.cs
@@ -6,23 +6,42 @@ public class RotateToPart : MoviePart {
 	[SerializeField]
 	private GameObject target;
 	[SerializeField]
+	private GameObject[] otherObjects;
+	[SerializeField]
 	private GameObject point;
 	[SerializeField]
 	private float seconds = 1.0f;
-	//FIXME:CameraRotateToのコピペ
+
 	public override IEnumerator MovieUpdate() {
-		var selfPos = target.transform.position;
-		var targetPos = point.transform.position;
-		targetPos.y = selfPos.y;
-		var dir = (targetPos - selfPos).normalized;
+		if(target == null) {
+			target = Camera.main.gameObject;
+		}
+		var pointPos = point.transform.position;
+		var pos = target.transform.position;
+		pointPos.y = pos.y;
 		var startRotation = target.transform.rotation;
-		var endRotation = Quaternion.LookRotation(dir);
+		var endRotation = Quaternion.LookRotation((pointPos - pos).normalized);
+		//他のオブジェクトの
+		var startRotationList = new Quaternion[otherObjects.Length];
+		var endRotationList = new Quaternion[otherObjects.Length];
+		for (int i = 0; i < otherObjects.Length; i++) {
+			startRotationList[i] = otherObjects[i].transform.rotation;
+		}
+		for (int i = 0; i < otherObjects.Length; i++) {
+			pos = otherObjects[i].transform.position;
+			pointPos.y = pos.y;
+			endRotationList[i] = Quaternion.LookRotation((pointPos - pos).normalized);
+		}
 		var offset = 0f;
 		while (offset < seconds) {
 			var t = Time.time;
 			yield return null;
 			offset += (Time.time - t);
 			target.transform.rotation = Quaternion.Slerp(startRotation, endRotation, offset / seconds);
+			for(int i=0; i<otherObjects.Length; i++) {
+				Debug.Log(otherObjects[i].name);
+				otherObjects[i].transform.rotation = Quaternion.Slerp(startRotationList[i], endRotationList[i], offset / seconds);
+			}
 		}
 	}
 }
