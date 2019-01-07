c51b71292d4029871a11c8f8cc4f61b8fd27d71e
matsumoto <matsumoto.k017a1377@gmail.com>
2019-01-07 17:56:34 +0900

    一連の流れの作成

--- Kimetu/Assets/Script/TimeLine/OpeningMovie.cs
+++ Kimetu/Assets/Script/TimeLine/OpeningMovie.cs
@@ -1,14 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Playables;
 
 public class OpeningMovie : MonoBehaviour {
-	[SerializeField]
+	[SerializeField, Header("ムービーを終了するボタン")]
 	private InputMap.Type[] forcedTerminationButtons;
 	private ChangeScene changeScene;
+	[SerializeField]
+	private PlayableDirector playableDirector;
+	private bool isEnd; //終了しているか
+
 	// Use this for initialization
 	void Start() {
 		changeScene = GetComponent<ChangeScene>();
+		Assert.IsNotNull(changeScene, "ChangeSceneが存在しません。");
+		//ムービー終了時のコールバック
+		playableDirector.stopped += MovieStop;
+		isEnd = false;
+	}
+
+	/// <summary>
+	/// ムービー終了時のコールバック
+	/// </summary>
+	/// <param name="obj"></param>
+	private void MovieStop(PlayableDirector obj) {
+		if (obj == playableDirector) {
+			ChangeNextScene();
+		}
 	}
 
 	// Update is called once per frame
@@ -21,7 +41,12 @@ public class OpeningMovie : MonoBehaviour {
 		}
 	}
 
+	/// <summary>
+	/// シーン変更
+	/// </summary>
 	private void ChangeNextScene() {
+		if (isEnd) return;
+		isEnd = true;
 		StartCoroutine(changeScene.OnExecute());
 	}
 }
