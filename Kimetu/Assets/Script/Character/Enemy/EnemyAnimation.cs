cd15cfa3a6b63495383c7e23b7d7788ef4bf5d7e
Koya <koya0306@outlook.jp>
2019-01-08 19:56:15 +0900

    刹那の二連撃、三連撃に声を追加

--- Kimetu/Assets/Script/Character/Enemy/EnemyAnimation.cs
+++ Kimetu/Assets/Script/Character/Enemy/EnemyAnimation.cs
@@ -7,6 +7,21 @@ using UnityEngine.Assertions;
 
 public class EnemyAnimation : CharacterAnimation {
 	public Animator anim { get { return animator; } }
+
+	[SerializeField]
+	private AudioSource audioSource;
+
+	private bool pauseVoice;
+
+	public enum Voice {
+		SetunaRen2Attack01,
+		SetunaRen2Attack02,
+		SetunaRen3Attack01,
+		SetunaRen3Attack02,
+		SetunaRen3Attack03
+	}
+
+
 	public void StartRunAnimation() {
 		animator.SetBool("Run", true);
 	}
@@ -45,4 +60,38 @@ public class EnemyAnimation : CharacterAnimation {
 		animator.SetTrigger("Repl");
 	}
 
+	public void PlayEnemyVoice(Voice v) {
+		switch(v) {
+			case Voice.SetunaRen2Attack01:
+				PlayVoice(AudioManager.Instance.GetSE(AudioName.setuna_ha_niren_01.String()));
+				break;
+			case Voice.SetunaRen2Attack02:
+				PlayVoice(AudioManager.Instance.GetSE(AudioName.setuna_ha_niren_01.String()));
+				break;
+			case Voice.SetunaRen3Attack01:
+				PlayVoice(AudioManager.Instance.GetSE(AudioName.setuna_hu_sanren_02.String()));
+				break;
+			case Voice.SetunaRen3Attack02:
+				PlayVoice(AudioManager.Instance.GetSE(AudioName.setuna_hu_sanren_02.String()));
+				break;
+			case Voice.SetunaRen3Attack03:
+				PlayVoice(AudioManager.Instance.GetSE(AudioName.setuna_hu_sanren_02.String()));
+				break;
+		}
+	}
+
+	private void PlayVoice(AudioClip clip) {
+		if(pauseVoice) {
+			audioSource.UnPause();
+			pauseVoice = false;
+		} else {
+			audioSource.clip = clip;
+			audioSource.Play();
+		}
+	}
+
+	public void PauseEnemyVoice() {
+		audioSource.Pause();
+		this.pauseVoice = true;
+	}
 }
