065a0c3f778673c228d8ff118b00fe1020e0de8a
Koya <koya0306@outlook.jp>
2019-01-09 19:38:22 +0900

    全ての鬼が攻撃前に唸り声を上げるように

--- Kimetu/Assets/Script/Character/Enemy/Action/AttackAction.cs
+++ Kimetu/Assets/Script/Character/Enemy/Action/AttackAction.cs
@@ -7,13 +7,19 @@ public class AttackAction : ActionBase {
 	[SerializeField]
 	private EnemyAttack enemyAttack;
 	private GameObject player;
+	private bool isSetuna;
 
 	protected override void Start() {
 		base.Start();
 		player = GetPlayer();
+		this.isSetuna = GetComponentInParent<LastBossAI>();
 	}
 
 	public override IEnumerator Action() {
+		if(!isSetuna && !SceneChanger.Instance().isChanging) {
+			AudioManager.Instance.PlayEnemySE(AudioName.oni_oaa_preAttack_03.String());
+			yield return new WaitForSeconds(1f);
+		}
 		yield return enemyAttack.Attack();
 	}
 
