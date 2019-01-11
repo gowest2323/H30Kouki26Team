f13f663ffa6aa864fe63833873aa58479b4f55ba
matsumoto <matsumoto.k017a1377@gmail.com>
2019-01-11 12:52:34 +0900

    雷攻撃の修正

---
+++ Kimetu/Assets/Script/Util/TriggerHitSender.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// コライダーのトリガーの衝突通知
+/// </summary>
+public class TriggerHitSender : MonoBehaviour {
+	//interfaceはインスペクターから編集できないため使用側がrecieverにthisをセットする
+	public IColliderHitReciever reciever { get; set; }
+
+	private void OnTriggerEnter(Collider other) {
+		reciever.RecieveOnTriggerEnter(other);
+	}
+
+	private void OnTriggerExit(Collider other) {
+		reciever.RecieveOnTriggerExit(other);
+	}
+}
