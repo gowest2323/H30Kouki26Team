34b125bdb15affa691cad189a31850d4fe02055b
Koya <koya0306@outlook.jp>
2019-01-28 15:34:28 +0900

    弾きHookを追加

---
+++ Kimetu/Assets/Script/Events/KioniReplHook.cs
@@ -0,0 +1,37 @@
+﻿//AUTO-GENERATED-CODE
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UniRx;
+
+[DisallowMultipleComponent]
+public class KioniReplHook : MonoBehaviour, IEventHook {
+	public bool animationNow { private set; get; }
+	public IObservable<bool> trigger { get { return subject; }}
+	private Subject<bool> subject;
+
+	void Awake() {
+		this.subject = new Subject<bool>();
+	}
+
+	void Start() {
+	}
+
+	public void KioniReplStart() {
+		Debug.Log("KioniReplStart");
+		this.animationNow = true;
+		subject.OnNext(true);
+	}
+
+	public void KioniReplEnd() {
+		Debug.Log("KioniReplEnd");
+		this.animationNow = false;
+		subject.OnNext(false);
+	}
+
+	public IEnumerator Wait() {
+		yield return new WaitWhile(() => !animationNow);
+		yield return new WaitWhile(() => animationNow);
+	}
+}
