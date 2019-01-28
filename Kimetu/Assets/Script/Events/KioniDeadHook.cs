05a1ce92ac9eef04595bdad68b8a1ce9bd713d99
Koya <koya0306@outlook.jp>
2019-01-28 16:44:58 +0900

    コミットもれ

---
+++ Kimetu/Assets/Script/Events/KioniDeadHook.cs
@@ -0,0 +1,37 @@
+﻿//AUTO-GENERATED-CODE
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UniRx;
+
+[DisallowMultipleComponent]
+public class KioniDeadHook : MonoBehaviour, IEventHook {
+	public bool animationNow { private set; get; }
+	public IObservable<bool> trigger { get { return subject; }}
+	private Subject<bool> subject;
+
+	void Awake() {
+		this.subject = new Subject<bool>();
+	}
+
+	void Start() {
+	}
+
+	public void KioniDeadStart() {
+		Debug.Log("KioniDeadStart");
+		this.animationNow = true;
+		subject.OnNext(true);
+	}
+
+	public void KioniDeadEnd() {
+		Debug.Log("KioniDeadEnd");
+		this.animationNow = false;
+		subject.OnNext(false);
+	}
+
+	public IEnumerator Wait() {
+		yield return new WaitWhile(() => !animationNow);
+		yield return new WaitWhile(() => animationNow);
+	}
+}
