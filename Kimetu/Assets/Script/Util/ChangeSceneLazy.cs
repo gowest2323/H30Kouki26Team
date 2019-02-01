b0bde1be91ffc10136e7934cffdc2b9222cf8547
Koya <koya0306@outlook.jp>
2019-02-02 00:38:50 +0900

    数秒待ってからシーンを切り替えるスクリプト追加

---
+++ Kimetu/Assets/Script/Util/ChangeSceneLazy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSceneLazy : MonoBehaviour {
+	[SerializeField]
+	private SceneName name;
+
+	[SerializeField]
+	private float wait = 1f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(Wait());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator Wait() {
+		yield return new WaitForSeconds(wait);
+		SceneChanger.Instance().Change(name, new FadeData(1, 1, Color.black));
+	}
+}
